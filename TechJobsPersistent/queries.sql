c10543b2bdd27563b2c799646e4603c5bd80d639
Johnathan <johnathan.m.crisp@gmail.com>
2020-08-04 10:38:10 -0500

    Finished

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,13 @@
 --Part 1
+--Id - int 
+--Name - longtext
+--EmployerId - int
 
 --Part 2
 
+-- SELECT Name FROM employers WHERE Location = "St. Louis City"
+
 --Part 3
+
+-- SELECT * FROM Skills INNER JOIN JobSkills ON Skills.Id = JobSkills.SkillId WHERE JobSkills.JobId IS NOT NULL ORDER BY NAME ASC;
 
